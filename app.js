76ffdaf71d0539e52d25c2d7dc7bc364c8f10b4c
Xoerte <jaimefabri037@gmail.com>
2022-08-22 13:06:46 -0300

    Commit sobre comentarios Javascript

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var VolverAcom = document.getElementById("b-2");
 var Volver = document.getElementById("b-1");
 var Verificar = document.getElementById("b-3")
 
-
+//FUNCIÓN PARA VERIFICAR QUE SOLO SEAN LETRAS MAYÚSCULAS
   const Numeros = (string) => {
   	var out = ""
   	var filtro = "ABCDEFGHIJKLMNOPQRSTUWYZX"
@@ -19,7 +19,7 @@ var Verificar = document.getElementById("b-3")
   }
 
 
-
+//FUNCIÓN PARA VOLVER A COMENZAR EL JUEGO DE LA SECCIÓN DE JUEGO
 VolverAcom.addEventListener("click", () =>{
     
     String.prototype.replaceAt=function(index, character) { return this.substring(0, index) + character + this.substring(index+character.length); }
@@ -44,7 +44,7 @@ VolverAcom.addEventListener("click", () =>{
 
 
 
-
+//FUNCIÓN DEL BOTON VOLVER DE LA SECCIÓN DE AGREGAR PALABRA
 Volver.addEventListener("click",function(){
 
 	document.getElementById("output").style.display = "none"
@@ -68,7 +68,7 @@ Volver.addEventListener("click",function(){
 	document.getElementById("ahorcado").src = "Personaje/Imagen1.png"
 
 })
-
+//FUNCIÓN PARA IR A LA SECCIÓN DE AGREGAR PALABRA
 botonAgregarPal.addEventListener("click",function() {
 	
 	document.getElementById("container").style.display = "initial";
@@ -79,7 +79,7 @@ botonAgregarPal.addEventListener("click",function() {
 	document.getElementById("bto-2").style.display = "none";
 })
 
-
+//FUNCIÓN PARA GUARDAR LA PALABRA TEMPORALMENTE
 var x;
 botonGuardar.addEventListener("click",function(){
 	msj("GUARDADA","La palabra fue guardada temporalmente")
@@ -89,6 +89,7 @@ botonGuardar.addEventListener("click",function(){
 	console.log(palabras)
 })
 
+//FUNCIÓN PARA LOS MENSAJES DE GANAR,PERDER Y GUARDADODE PALABRA
 function borrar(id) {var elem = document.getElementById(id); return elem.parentNode.removeChild(elem);}
 function msj(titulo, contenido, idioma) {
 	var padre = document.createElement('div');
@@ -114,7 +115,7 @@ function msj(titulo, contenido, idioma) {
 	var len = document.getElementById("output")
 	var contadorFallos = 1;
 
-
+//BÓTON PARA VERIFICAR LA RESPUESTA DEL USUARIO
 Verificar.addEventListener("click", () => {
 	const letra = document.getElementById("t-3").value
 
@@ -139,7 +140,8 @@ if (palabraConGuiones.search("_")==-1)  {
 }
 
 if(haFallado) {
-	
+   if(letra !== "")	{
+
 	contadorFallos++
 	document.querySelector("img").src = `Personaje/Imagen${contadorFallos}.png`
 
@@ -161,12 +163,16 @@ if(haFallado) {
 	    	palabraConGuiones = palabra.replace(/./g,"_ ")
 	    	msj("Perdiste","Cierre el mensaje y vuelva a comenzar")
 	}  
+// SEGUNDO IF
+} 
+// SEGUNDO IF
 }
 
 document.getElementById("output").innerHTML = palabraConGuiones
 document.getElementById("t-3").value = ""
 })
 
+//FUNCIÓN PARA COMENZAR EL JUEGO
 botonComenzar.addEventListener("click",function() {
 	
 	usuario = document.getElementById("tex1").value
@@ -184,6 +190,7 @@ botonComenzar.addEventListener("click",function() {
 	document.querySelector("img").style.display = "initial"
 })
 
+//BÓTON PARA CANCELAR
 botonCancelar.addEventListener("click",function() {
 
 	document.getElementById("container").style.display = "none";
@@ -193,7 +200,7 @@ botonCancelar.addEventListener("click",function() {
 	document.getElementById("bto-1").style.display = "initial";
 	document.getElementById("bto-2").style.display = "initial";
 })
-
+//
 
 
 
